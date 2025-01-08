088d7c42fd6107b5f079cc8bbe73c92bd689ab58
Shayaan <shayaanmomin.sm@gmail.com>
2025-01-08 16:03:24 +0530

    Custom Themeing

---
+++ web/themes/custom/moviesflix/js/moviesflix.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * moviesflix behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.moviesflix = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
